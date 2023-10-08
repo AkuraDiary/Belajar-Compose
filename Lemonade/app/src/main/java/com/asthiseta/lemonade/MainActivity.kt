4b4cb2ea7f1c9dbad9b9fc5735759de6d41a20cf
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-10-08 13:39:30 +0700

    added images assets

--- Lemonade/app/src/main/java/com/asthiseta/lemonade/MainActivity.kt
+++ Lemonade/app/src/main/java/com/asthiseta/lemonade/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.asthiseta.lemonade
 
+
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -9,6 +10,7 @@ import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.asthiseta.lemonade.ui.theme.LemonadeTheme
 
@@ -20,9 +22,9 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = Color.White
                 ) {
-                    Greeting("Android")
+                    LemonadePreview()
                 }
             }
         }
@@ -37,9 +39,9 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
-@Preview(showBackground = true)
+@Preview(showBackground = true, showSystemUi = true)
 @Composable
-fun GreetingPreview() {
+fun LemonadePreview() {
     LemonadeTheme {
         Greeting("Android")
     }
