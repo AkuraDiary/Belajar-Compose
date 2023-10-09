1af83da40bbb3dda9104df20e833d70cb906ae02
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-10-09 19:57:17 +0700

    added leadin icon

--- TipCalculator/app/src/main/java/com/asthiseta/tipcalculator/MainActivity.kt
+++ TipCalculator/app/src/main/java/com/asthiseta/tipcalculator/MainActivity.kt
@@ -5,18 +5,23 @@ package com.asthiseta.tipcalculator
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.material3.Icon
 import androidx.compose.ui.unit.dp
 import com.asthiseta.tipcalculator.ui.theme.TipCalculatorTheme
 import java.text.NumberFormat
 import androidx.compose.runtime.*
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.sp
@@ -54,7 +59,7 @@ private fun calculateTip(
 @Composable
 fun TipLayout() {
     Column(
-        modifier = Modifier.padding(40.dp),
+        modifier = Modifier.padding(40.dp).verticalScroll(rememberScrollState()),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
@@ -83,6 +88,7 @@ fun TipLayout() {
                 keyboardType = KeyboardType.Number,
                 imeAction = ImeAction.Next
             ),
+            leadingIcon =  R.drawable.money,
             modifier = Modifier.padding(bottom = 32.dp).fillMaxWidth()
         )
 
@@ -92,6 +98,7 @@ fun TipLayout() {
             onValueChange = { text ->
                 tipInput = text
             },
+            leadingIcon =  R.drawable.percent,
             keyboardOptions = KeyboardOptions(
                 keyboardType = KeyboardType.Number,
                 imeAction = ImeAction.Done
@@ -122,6 +129,7 @@ fun TipLayout() {
 @Composable
 fun EditNumberField(
     @StringRes label: Int,
+    @DrawableRes leadingIcon : Int,
     value: String,
     onValueChange: (String) -> Unit,
     keyboardOptions: KeyboardOptions,
@@ -130,6 +138,7 @@ fun EditNumberField(
 
     TextField(
         value = value,
+        leadingIcon = {Icon(painter = painterResource(id = leadingIcon), null)},
         onValueChange = onValueChange,
 //        onValueChange = {
 //            // TODO
