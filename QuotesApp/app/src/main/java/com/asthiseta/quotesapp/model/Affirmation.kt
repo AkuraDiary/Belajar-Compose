9a78489c335f5740f9b4e4076d8e8b25c983a35b
Muhammad Asthi Seta Ari Yuwana <asthiseta@gmail.com>
2023-10-10 07:03:40 +0700

    quotes app starter adding assets

---
+++ QuotesApp/app/src/main/java/com/asthiseta/quotesapp/model/Affirmation.kt
@@ -0,0 +1,9 @@
+package com.asthiseta.quotesapp.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Affirmation(
+    @StringRes val stringResourceId: Int,
+    @DrawableRes val imageResourceId: Int
+)
